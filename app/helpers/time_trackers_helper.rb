aaafaf95971450d42af6a6b42830c30ca627eb79
Martin Denizet <martin.denizet@supinfo.com>
2011-08-19 03:13:23 -0700

    Extended global permissions. * Possibility to disable the plugin for certain users. * Fix potential security issue

--- app/helpers/time_trackers_helper.rb
+++ app/helpers/time_trackers_helper.rb
@@ -1,9 +1,13 @@
 module TimeTrackersHelper
-    def issue_from_id(issue_id)
-        Issue.find(:first, :conditions => { :id => issue_id })
-    end
+  def issue_from_id(issue_id)
+    Issue.find(:first, :conditions => { :id => issue_id })
+  end
 
-    def user_from_id(user_id)
-        User.find(:first, :conditions => { :id => user_id })
-    end
+  def user_from_id(user_id)
+    User.find(:first, :conditions => { :id => user_id })
+  end
+
+  def authorized_globally(controller,action)
+    User.current.allowed_to?({:controller => controller, :action => action},nil, :global => true)
+  end
 end
--- init.rb
+++ init.rb
@@ -3,24 +3,38 @@ require 'redmine'
 require_dependency 'time_tracker_hooks'
 
 Redmine::Plugin.register :redmine_time_tracker do
-    name 'Redmine Time Tracker plugin'
-    author 'Jérémie Delaitre'
-    description 'This is a plugin to track time in Redmine'
-    version '0.4'
+  name 'Redmine Time Tracker plugin'
+  author 'Jérémie Delaitre'
+  description 'This is a plugin to track time in Redmine'
+  version '0.5-dev'
 
-    requires_redmine :version_or_higher => '1.1.0'
+  requires_redmine :version_or_higher => '1.1.0'
 
-    settings :default => { 'refresh_rate' => '60', 'status_transitions' => {} }, :partial => 'settings/time_tracker'
+  settings :default => { 'refresh_rate' => '60', 'status_transitions' => {} }, :partial => 'settings/time_tracker'
 
+  project_module :time_tracker_plugin do
     permission :view_others_time_trackers, :time_trackers => :index
     permission :delete_others_time_trackers, :time_trackers => :delete
 
-    menu :account_menu, :time_tracker_menu, '',
-        {
-            :caption => '',
-            :html => { :id => 'time-tracker-menu' },
-            :first => true,
-            :param => :project_id,
-            :if => Proc.new { User.current.logged? }
-        }
+
+    permission :use_time_tracker, {:time_trackers =>
+        [:start,:resume,:suspend,:stop,:render_menu,:current,:apply_status_transition]
+    }
+
+    permission :manage_time_trackers_status_transition, {:time_trackers =>
+        [:add_status_transition,:delete_status_transition]
+    }
+  end
+
+  menu :account_menu, :time_tracker_menu, '',
+    {
+    :caption => '',
+    :html => { :id => 'time-tracker-menu' },
+    :first => true,
+    :param => :project_id,
+    :if => Proc.new {
+      User.current.allowed_to?({:controller => 'time_trackers', :action => 'render_menu'},nil, :global => true)
+    }
+  }
+  
 end
